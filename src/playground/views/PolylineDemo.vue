b48e5b57f09d6fc75af77cc5cd4d19474a980e13
rongzedong <rongzedong@users.noreply.github.com>
2025-06-07 21:14:23 +0800

    Update PolylineDemo.vue

--- src/playground/views/PolylineDemo.vue
+++ src/playground/views/PolylineDemo.vue
@@ -7,18 +7,27 @@
     ></l-tile-layer>
 
     <l-polyline
-      :lat-lngs="[
-        [47.334852, -1.509485],
-        [47.342596, -1.328731],
-        [47.241487, -1.190568],
-        [47.234787, -1.358337],
-      ]"
+      :edit=true   
+      v-model:lat-lngs="latlngs" 
       color="green"
     />
   </l-map>
 </template>
 <script lang="ts">
 import { LMap, LPolyline, LTileLayer } from "@src/components";
+
+import { ref, watch } from "vue";
+
+const latlngs = ref([
+  [47.334852, -1.509485],
+        [47.342596, -1.328731],
+        [47.241487, -1.190568],
+        [47.234787, -1.358337],
+]);
+
+watch(latlngs, (newVal) => {
+  console.log('latlngs changed:', JSON.stringify(newVal))
+});
 
 export default {
   components: {
@@ -28,10 +37,9 @@ export default {
   },
   data() {
     return {
-      zoom: 9,
+      zoom: 5,
+      latlngs: latlngs
     };
   },
 };
 </script>
-
-<style></style>
