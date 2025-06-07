5dd52233d110c48f5db722534909b9362c6f49b6
rongzedong <rongzedong@users.noreply.github.com>
2025-06-07 21:10:25 +0800

    Update LPolyline.vue

--- src/components/LPolyline.vue
+++ src/components/LPolyline.vue
@@ -1,107 +1,92 @@
 <script lang="ts">
-import type L from "leaflet";
-import { debounce } from "ts-debounce";
-import {  defineComponent,  inject,  markRaw,  onMounted,  ref,  watch, nextTick, onBeforeUnmount, onUnmounted} from "vue";
+import L from "leaflet";
+import { defineComponent, markRaw, onMounted, ref, watch, onUnmounted } from "vue";
+import { AddLayerInjection, RemoveLayerInjection } from "@src/types/injectionKeys";
 import { setupPolyline, polylineProps } from "@src/functions/polyline";
 import { render } from "@src/functions/layer";
-import {
-  AddLayerInjection,
-  RemoveLayerInjection,
-  CanSetParentHtmlInjection,
-  SetIconInjection,
-  SetParentHtmlInjection,
-  UseGlobalLeafletInjection,
-} from "@src/types/injectionKeys";
-import {
-  WINDOW_OR_GLOBAL,
-  assertInject,
-  cancelDebounces,
-  isFunction,
-  propsBinder,
-  remapEvents,
-} from "@src/utils.js";
-
-
+import { assertInject } from "@src/utils.js";
 
 export default defineComponent({
   name: "LPolyline",
   props: polylineProps,
+  emits: ['update:latLngs'],
   setup(props, context) {
     const leafletObject = ref<L.Polyline>();
-    const ready = ref(false);
+    const editLayerGroup = ref<L.LayerGroup | null>(null);
     const addLayer = assertInject(AddLayerInjection);
     const removeLayer = assertInject(RemoveLayerInjection);
 
     const { options, methods } = setupPolyline(props, leafletObject, context);
 
-    const eventHandlers = {
-      moveHandler: debounce(methods.latLngSync),
-    };
-
     function getPoints() {
-      if (props.coordinates && props.coordinates.length) return props.coordinates;
       if (props.latLngs && props.latLngs.length) return props.latLngs;
       return [];
     }
 
+    // ========== 新增编辑模式相关 ==========
+    function addEditLayerGroup() {
+      if (editLayerGroup.value) return;
+      const latlngs = [...getPoints()];
+      const group = L.layerGroup();
+      latlngs.forEach((latlng, idx) => {
+        const marker = L.marker(latlng, { draggable: true });
+        marker.on('drag', (e) => {
+          latlngs[idx] = [e.latlng.lat, e.latlng.lng];
+          leafletObject.value?.setLatLngs(latlngs);
+        });
+        marker.on('dragend', () => {
+          context.emit('update:latLngs', latlngs);
+        });
+        group.addLayer(marker);
+      });
+      editLayerGroup.value = group;
+      addLayer({ leafletObject: group });
+    }
+
+    function removeEditLayerGroup() {
+      if (editLayerGroup.value) {
+        removeLayer({ leafletObject: editLayerGroup.value });
+        editLayerGroup.value = null;
+      }
+    }
+    // =====================================
+
     onMounted(() => {
-      // TODO L.layerGroup([marker1, marker2]) 如果是编辑模式，就返回 带标记的群组
-      // 或者在 addEditHandles 和 removeEditHandles 的时候，用 addLayer 方式来实现
       leafletObject.value = markRaw(L.polyline(getPoints(), options));
-      const { listeners } = remapEvents(context.attrs);
-      leafletObject.value.on(listeners);
-      leafletObject.value.on("move", eventHandlers.moveHandler);
-      propsBinder(methods, leafletObject.value, props);
       addLayer({
         ...props,
         ...methods,
         leafletObject: leafletObject.value,
       });
 
-       watch(
-        () => {
-          // console.log(props.edit);
-          return props.edit;
-        },
+      watch(
+        () => props.edit,
         (val) => {
-          // console.log(val);
           if (val) {
-            var handle = methods.removeEditHandles();
-            addLayer({
-            ...props,
-            ...methods,
-            leafletObject: handle.value
-            })
-
+            addEditLayerGroup();
+          } else {
+            removeEditLayerGroup();
           }
-          else ;
         },
         { immediate: true }
       );
       watch(
         () => getPoints(),
         () => {
+          leafletObject.value?.setLatLngs(getPoints());
           if (props.edit) {
-            removeLayer({})
-            methods.removeEditHandles();
-            var handle = methods.removeEditHandles();
-            addLayer({
-            ...props,
-            ...methods,
-            leafletObject: handle.value
-            })
+            removeEditLayerGroup();
+            addEditLayerGroup();
           }
         }
       );
-
-
-      ready.value = true;
-      nextTick(() => context.emit("ready", leafletObject.value));
     });
 
-    onBeforeUnmount(() => cancelDebounces(eventHandlers));
+    onUnmounted(() => {
+      removeEditLayerGroup();
+    });
 
-    return { ready, leafletObject };
+    return { leafletObject, ready: true };
   },
   render() {
     return render(this.ready, this.$slots);
