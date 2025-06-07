7d109832e39b58d11750c4ee8f0ddc5c64b19a87
rongzedong <rongzedong@users.noreply.github.com>
2025-06-07 19:54:27 +0800

    Update LPolyline.vue

--- src/components/LPolyline.vue
+++ src/components/LPolyline.vue
@@ -1,23 +1,42 @@
 <script lang="ts">
-import L from "leaflet";
-import { defineComponent, inject, markRaw, onMounted, ref, watch, onUnmounted } from "vue";
-import { LMapInjectionKey, AddLayerInjection } from "@src/types/injectionKeys";
+import type L from "leaflet";
+import { debounce } from "ts-debounce";
+import {  defineComponent,  inject,  markRaw,  onMounted,  ref,  watch, nextTick, onBeforeUnmount, onUnmounted} from "vue";
 import { setupPolyline, polylineProps } from "@src/functions/polyline";
 import { render } from "@src/functions/layer";
-import { assertInject } from "@src/utils.js";
+import {
+  AddLayerInjection,
+  RemoveLayerInjection,
+  CanSetParentHtmlInjection,
+  SetIconInjection,
+  SetParentHtmlInjection,
+  UseGlobalLeafletInjection,
+} from "@src/types/injectionKeys";
+import {
+  WINDOW_OR_GLOBAL,
+  assertInject,
+  cancelDebounces,
+  isFunction,
+  propsBinder,
+  remapEvents,
+} from "@src/utils.js";
 
-/**
- * Polyline component, lets you add polylines to the map
- */
+
+
 export default defineComponent({
   name: "LPolyline",
   props: polylineProps,
   setup(props, context) {
     const leafletObject = ref<L.Polyline>();
-    const map = inject<any>(LMapInjectionKey);
+    const ready = ref(false);
     const addLayer = assertInject(AddLayerInjection);
+    const removeLayer = assertInject(RemoveLayerInjection);
 
     const { options, methods } = setupPolyline(props, leafletObject, context);
+
+    const eventHandlers = {
+      moveHandler: debounce(methods.latLngSync),
+    };
 
     function getPoints() {
       if (props.coordinates && props.coordinates.length) return props.coordinates;
@@ -26,42 +45,65 @@ export default defineComponent({
     }
 
     onMounted(() => {
+      // TODO L.layerGroup([marker1, marker2]) 如果是编辑模式，就返回 带标记的群组
+      // 或者在 addEditHandles 和 removeEditHandles 的时候，用 addLayer 方式来实现
       leafletObject.value = markRaw(L.polyline(getPoints(), options));
+      const { listeners } = remapEvents(context.attrs);
+      leafletObject.value.on(listeners);
+      leafletObject.value.on("move", eventHandlers.moveHandler);
+      propsBinder(methods, leafletObject.value, props);
       addLayer({
         ...props,
         ...methods,
         leafletObject: leafletObject.value,
       });
 
-      // Watch for editing state
-      watch(
-        () => props.edit,
+       watch(
+        () => {
+          // console.log(props.edit);
+          return props.edit;
+        },
         (val) => {
-          if (val && map?.value) methods.addEditHandles(map.value);
-          else methods.removeEditHandles();
+          // console.log(val);
+          if (val) {
+            var handle = methods.removeEditHandles();
+            addLayer({
+            ...props,
+            ...methods,
+            leafletObject: handle.value
+            })
+
+          }
+          else ;
         },
         { immediate: true }
       );
-      // Watch for points changes
       watch(
         () => getPoints(),
         () => {
-          if (props.edit && map?.value) {
+          if (props.edit) {
+            removeLayer({})
             methods.removeEditHandles();
-            methods.addEditHandles(map.value);
+            var handle = methods.removeEditHandles();
+            addLayer({
+            ...props,
+            ...methods,
+            leafletObject: handle.value
+            })
           }
         }
       );
+
+
+      ready.value = true;
+      nextTick(() => context.emit("ready", leafletObject.value));
     });
 
-    onUnmounted(() => {
-      methods.removeEditHandles();
-    });
+    onBeforeUnmount(() => cancelDebounces(eventHandlers));
 
-    return { leafletObject, ready: true };
+    return { ready, leafletObject };
   },
   render() {
-    // 这里直接参考 LMarker 的 render模式
     return render(this.ready, this.$slots);
   },
 });
