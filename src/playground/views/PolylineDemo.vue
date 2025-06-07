ceea84819a1294752a3a392868a6ded20876b99c
rongzedong <rongzedong@users.noreply.github.com>
2025-06-07 21:49:32 +0800

    Update PolylineDemo.vue

--- src/playground/views/PolylineDemo.vue
+++ src/playground/views/PolylineDemo.vue
@@ -1,5 +1,5 @@
 <template>
-  <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+  <l-map ref="map" v-model:zoom="zoom" :center="[25.774, -80.19]">
     <l-tile-layer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       layer-type="base"
@@ -18,11 +18,11 @@ import { LMap, LPolyline, LTileLayer } from "@src/components";
 
 import { ref, watch } from "vue";
 
-const latlngs = ref([
-  [47.334852, -1.509485],
-        [47.342596, -1.328731],
-        [47.241487, -1.190568],
-        [47.234787, -1.358337],
+const latlngs = ref<[number, number][]>([
+  [25.774, -80.19],
+  [18.466, -66.118],
+  [32.321, -64.757],
+  [25.774, -80.19],
 ]);
 
 watch(latlngs, (newVal) => {
