f608bc58e8f492fea38d0e98c082a76a33a8d165
rongzedong <rongzedong@users.noreply.github.com>
2025-06-08 09:48:45 +0800

    Update LRectangle.vue

--- src/components/LRectangle.vue
+++ src/components/LRectangle.vue
@@ -42,15 +42,9 @@ export default defineComponent({
         ? WINDOW_OR_GLOBAL.L
         : await import("leaflet/dist/leaflet-src.esm");
 
-
-      const latLngs = props.latLngs ?? [];
       const bounds = props.bounds
         ? latLngBounds(props.bounds)
-        : latLngBounds(
-            Array.isArray(latLngs[0])
-              ? (latLngs[0] as L.LatLngExpression[])
-              : (latLngs as L.LatLngExpression[])
-      );
+        : latLngBounds(props.latLngs || []);
       leafletObject.value = markRaw<L.Rectangle>(rectangle(bounds, options));
 
       const { listeners } = remapEvents(context.attrs);
